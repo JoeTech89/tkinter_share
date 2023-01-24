b28cca6dd62c07a13295b4cba7b770e0346f5068
JoeTech89 <arjoe89@gmail.com>
2023-01-24 14:27:06 -0500

    Add files via upload

--- TKINTER SHARE.py
+++ TKINTER SHARE.py
@@ -1,5 +1,29 @@
 import tkinter as tk
 
+def check_key(key:str,against:list):
+	count=0
+	for k in against:
+		if k==key or k==key+str(count):
+			count+=1
+			print('check key:',k,' found:',count)
+	if count>0:
+		key=key+str(count)
+	return key
+
+def check_name(name:str,against:list,dash=''):
+	count=0
+	for n in against:
+		if n==name or n==name+dash+str(count):
+			count+=1
+	if count>0:
+		name=name+dash+str(count)
+	return name
+
+def step_grid(context):
+	context.curColumn+=1
+	if context.curColumn>context.maxColumn:
+		context.curRow+=1
+
 class Menu(tk.Menu):
 	def __init__(self,container,**kwargs):
 		super().__init__(container,**kwargs)
@@ -9,7 +33,7 @@ class Menu(tk.Menu):
 		self.itemCount=0
 
 	def add_items(self,items:list)->int:
-	#items=[{'t':type,'l':label,'c':command,'m':menu}]
+	#items=[{'t':type,'l':label,'c':command,'m':submenu},{'t':type,'l':label,'c':command,'m':submenu}]
 		for item in items:
 			if item['t']=='separator' or item['t']=='s':
 				self.add_separator()
@@ -42,3 +66,45 @@ class Menu(tk.Menu):
 				self.entryconfig(self.itemCount,accelerator=item['acel'])
 		self.itemCount+=1#|Needs to be inside loop, to iterate
 		print('Added items',self.itemCount,' to menu')
+
+def add_panel(self,panel,key='',style='normal'):
+	key=check_key(key,self.objects)
+
+	if self.gridtype==-1:self.gridtype=0
+
+	if self.gridtype==0:
+		fill='none'
+		expand=False
+		if style=='normal':
+			fill='none'
+			expand=False
+		if style=='full':
+			fill='both'
+			expand=True
+		if style=='horizontal':
+			fill='x'
+			expand=True
+		if style=='vertical':
+			fill='y'
+			expand=True
+		panel.base.pack(fill=fill,expand=expand,padx=2,pady=2)
+		print('packed:',panel,' to window')
+	if self.gridtype==1:
+		anchor='nw'
+		sticky='nw'
+		if style=='normal':
+			anchor='nw'
+			sticky='nw'
+		if style=='full':
+			anchor='nw'
+			sticky='nsew'
+		if style=='horizontal':
+			anchor='nw'
+			sticky='nsew'
+		if style=='vertical':
+			anchor='nw'
+			sticky='nsew'
+		panel.base.grid(row=self.curRow,column=self.curColumn,anchor='nw',sticky='nsew',padx=2,pady=2)
+		step_grid(self)
+	self.objects[key]=panel
+	return key,panel
